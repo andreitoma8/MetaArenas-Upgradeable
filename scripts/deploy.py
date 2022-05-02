e8ef083d62e86993be9c1bb266f6a9a73f3ed748
andreitoma8 <andrei.toma8@yahoo.com>
2022-05-02 17:03:09 +0300

    main

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,41 +1,47 @@
 from brownie import (
+    Contract,
     MetaArenas,
     ArenasOld,
     EsportToken,
     ByteToken,
     MetaPasses,
+    ProxyAdmin,
+    TransparentUpgradeableProxy,
     accounts,
-    chain,
+    config,
 )
+from scripts.helpful_scripts import encode_function_data, upgrade
 
 
 def main():
     # Deloy
-    owner = accounts[0]
+    owner = accounts.add(config["wallets"]["from_key"])
+    # Deploy Proxi Admin
+    proxy_admin = ProxyAdmin.deploy({"from": owner})
     byte = ByteToken.deploy({"from": owner})
     esport = EsportToken.deploy({"from": owner})
     passes = MetaPasses.deploy({"from": owner})
     old_arenas = ArenasOld.deploy({"from": owner})
     old_arenas.mint(10, {"from": owner})
-    meta_arenas = MetaArenas.deploy(
-        old_arenas.address,
-        passes.address,
-        esport.address,
-        byte.address,
+    # Deploy the first MetaArenas implementation
+    implementation = MetaArenas.deploy(
         {"from": owner},
     )
+    # Encode the initializa function
+    encoded_initializer_function = encode_function_data(implementation.initialize)
+    proxy = TransparentUpgradeableProxy.deploy(
+        implementation.address,
+        proxy_admin.address,
+        encoded_initializer_function,
+        {"from": owner},
+    )
+    # Set Proxy ABI same as Implementation ABI
+    meta_arenas = Contract.from_abi("MetaArenas", proxy.address, MetaArenas.abi)
+    # Set the Address for interfaces in proxy
+    meta_arenas.setInterfaces(
+        old_arenas.address, passes.address, esport.address, {"from": owner}
+    )
     # Approve for Burn
-    approve_burn_tx = old_arenas.approve(meta_arenas.address, 1, {"from": owner})
+    approve_burn_tx = old_arenas.approve(meta_arenas.address, 0, {"from": owner})
     # Migrate Arena
-    migrate_tx = meta_arenas.migrateArena(1, {"from": owner})
-    # Stake Arena
-    stake_tx = meta_arenas.stakeArena(1, {"from": owner})
-    user_arenas_staked = meta_arenas.userStakedArenas(owner.address, {"from": owner})
-    print(user_arenas_staked)
-    # Forward in time
-    chain.mine(blocks=100, timedelta=173000)
-    # Assert accumulation of rewards and level
-    arena_stake_info = meta_arenas.arenaStakeInfo(1, {"from": owner})
-    print(arena_stake_info)
-    arena_details = meta_arenas.arenaDetails(1, {"from": owner})
-    print(arena_details)
+    migrate_tx = meta_arenas.migrateArena(0, {"from": owner})
